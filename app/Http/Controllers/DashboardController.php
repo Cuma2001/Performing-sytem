690f6e62ffa221b8558c56b903c5eadf78d8e591
Cuma2001 <157569913+Cuma2001@users.noreply.github.com>
2026-06-02 14:33:02 +0200

    Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\EmployeeKPI;
 use App\Models\KPI;
 use App\Models\Store;
 use App\Models\User;
@@ -13,15 +12,9 @@ class DashboardController extends Controller
     {
         $user = auth()->user();
 
-        $kpis = EmployeeKPI::where('user_id', $user->id)->get();
+        // Default score
+        $finalScore = 0;
 
-        $avgScore = $kpis->avg('score');
-
-        $salesScore = $avgScore * 0.8;
-        $generalScore = $avgScore * 0.2;
-
-        $finalScore = $salesScore + $generalScore;
-
-        return view('dashboard', compact('kpis','finalScore'));
+        return view('dashboard', compact('finalScore'));
     }
 }
